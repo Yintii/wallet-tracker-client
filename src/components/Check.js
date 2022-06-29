d9303a56e40664d06ed727ccebcac687d47b00a5
yintii <keleheart1@gmail.com>
2022-06-28 22:42:29 -0700

    made the front end for the /check route

--- src/components/Check.js
+++ src/components/Check.js
@@ -1,21 +1,47 @@
-import React from 'react'
-import { data } from '../testdata/testdata'
+import React, { useState, useEffect } from 'react'
+// import { data } from '../testdata/testdata'
 
 
 export const Check = () => {
 
+    const [data, setData] = useState('')
+
+
+    async function getData() {
+        let walletData = await fetch('http://localhost:5001/api/v1/accounts/check').then(response => response.json())
+        console.log(walletData)
+        setData(walletData)
+    }
+
     const RenderData = () => {
-        const renderedData = data.map(wallet => {
+        const renderedData = data.accountsList.map(account => {
             return (
-                <div>
-                    {wallet.name}: BTC:{wallet.btc}
-                </div>
+                <h1>
+                    {account.accountName}
+                    {account.wallets.map(wallet => {
+                        return (
+                            <div style={{ backgroundColor: "green" }}>
+                                <h3>{wallet.walletName}</h3>
+                                <h4>{wallet.walletType}</h4>
+                                <span>{wallet.walletXpub}</span>
+                                <hr />
+                            </div>
+                        )
+                    })}
+                </h1>
             )
         })
         return renderedData
     }
 
+    useEffect(() => {
+        getData()
+        console.log(data.accountsList[0].accountName)
+    }, [])
+
     return (
+
         <RenderData />
+
     )
 }
